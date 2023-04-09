bb1b17380d55ccf16b2011dcd9d0eea5acf8ec61
Ilya Zhalnin <mr.ilay333@gmail.com>
2023-04-10 01:25:05 +0300

    Fix authentication

--- jobs_api/common/dependencies/security.py
+++ jobs_api/common/dependencies/security.py
@@ -1,3 +1,17 @@
-from fastapi.security import HTTPBasic
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette import status
+import bcrypt
 
-auth = HTTPBasic()
+from jobs_api.controllers.user import UserController
+
+_security = HTTPBasic()
+
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(_security), user_controller: UserController = Depends()):
+    user = user_controller.get_by_email(credentials.username)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
+    if not bcrypt.checkpw(credentials.password.encode("utf-8"), user.password.encode("utf-8")):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
+    return user.id
