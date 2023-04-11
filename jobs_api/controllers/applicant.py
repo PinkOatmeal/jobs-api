58fa575e989e8159e22686e42fab76678fe3c640
Ilya Zhalnin <mr.ilay333@gmail.com>
2023-04-11 22:29:20 +0300

    Refactor ApplicantController

--- jobs_api/controllers/applicant.py
+++ jobs_api/controllers/applicant.py
@@ -1,14 +1,23 @@
 import bcrypt
+from fastapi import Depends
 from pydantic import BaseModel as BaseForm
-from sqlalchemy import select
+from sqlalchemy.orm import Session
 
 from jobs_api.api.applicants.forms import ApplicantSignUpForm
 from jobs_api.common.controllers import BaseController
+from jobs_api.common.dependencies.db import get_db
+from jobs_api.common.enums import Role
+from jobs_api.controllers.user import UserController
+from jobs_api.database import UserModel
 from jobs_api.database.applicant import ApplicantModel
 
 
 class ApplicantController(BaseController[ApplicantModel]):
     model = ApplicantModel
+
+    def __init__(self, session: Session = Depends(get_db), user_controller: UserController = Depends()):
+        super().__init__(session)
+        self._user_controller = user_controller
 
     def create(self, form: ApplicantSignUpForm) -> ApplicantModel:
         model = self.model(
@@ -26,9 +35,8 @@ class ApplicantController(BaseController[ApplicantModel]):
         self.session.refresh(model)
         return model
 
-    def get_by_email(self, email: str) -> ApplicantModel:
-        stmt = select(self.model).where(self.model.email == email)
-        return self.session.scalar(stmt)
+    def get_by_email(self, email: str) -> UserModel:
+        return self._user_controller.get_by_email(email)
 
     def update(self, _id: int, form: BaseForm):
         raise NotImplemented
