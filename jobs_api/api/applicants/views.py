d0de3a6602cb3b33d69ab12248127bfac42f60bb
Ilya Zhalnin <mr.ilay333@gmail.com>
2023-04-11 22:30:02 +0300

    Cosmetic on applicant views

--- jobs_api/api/applicants/views.py
+++ jobs_api/api/applicants/views.py
@@ -6,10 +6,10 @@ from jobs_api.api.user.dto import UserDTO
 from jobs_api.common.dependencies.security import authenticate
 from jobs_api.controllers.applicant import ApplicantController
 
-router = APIRouter(prefix="/applicants", tags=["Applicants"])
+router = APIRouter(prefix="/applicant", tags=["Applicants"])
 
 
-@router.post("/sign_up", response_model=SignUpResponse)
+@router.post("/sign_up", response_model=SignUpResponse, status_code=status.HTTP_201_CREATED)
 def sign_up(form: ApplicantSignUpForm = Body(...), controller: ApplicantController = Depends()):
     if controller.get_by_email(form.email):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with this email already exists")
