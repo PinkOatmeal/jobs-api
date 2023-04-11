47d10b41a59f3d772ff1493498fd0d0f1b5aeb11
Ilya Zhalnin <mr.ilay333@gmail.com>
2023-04-11 22:29:41 +0300

    Add employer views

---
+++ jobs_api/api/employers/schemas.py
@@ -0,0 +1,12 @@
+from jobs_api.common.enums import Role
+from jobs_api.common.schemas import BaseResponseSchema
+
+
+class SignUpResponse(BaseResponseSchema):
+    id: int
+    email: str
+
+
+class SignInResponse(BaseResponseSchema):
+    id: int
+    role: Role = Role.employer
---
+++ jobs_api/api/employers/views.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Body, Depends, HTTPException, status
+
+from jobs_api.api.employers.schemas import SignInResponse, SignUpResponse
+from jobs_api.api.employers.forms import EmployerSignUpForm
+from jobs_api.common.dependencies.security import authenticate
+from jobs_api.controllers.employer import EmployerController
+
+router = APIRouter(prefix="/employer", tags=["Employers"])
+
+
+@router.post("/sign_up", response_model=SignUpResponse, status_code=status.HTTP_201_CREATED)
+def sign_up(form: EmployerSignUpForm = Body(...), controller: EmployerController = Depends()):
+    if controller.get_by_email(form.email):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with this email already exists")
+    return controller.create(form)
+
+
+@router.post("/sign_in")
+def sign_in(user_id: int = Depends(authenticate)):
+    return SignInResponse(id=user_id)
